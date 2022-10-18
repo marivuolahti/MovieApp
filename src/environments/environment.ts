ef85bc929e0e9069acec0cac26368d042aa4670a
marivuolahti <78043421+marivuolahti@users.noreply.github.com>
2022-10-18 18:56:57 +0300

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '010c1d4ecefe5859dd172cfcefce9a36', // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
